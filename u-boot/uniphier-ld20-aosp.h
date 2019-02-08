95f4cbb86300262890004abc61462d9fcdfd999a
Masami Hiramatsu <masami.hiramatsu@linaro.org>
2019-02-08 22:02:46 +0900

    u-boot: Undefine CONFIG_PREBOOT for compatibility
    Since recent u-boot uniphier platform defines CONFIG_PREBOOT,
    undefine CONFIG_PREBOOT for compatibility and avoid warnings.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@linaro.org>

--- u-boot/uniphier-ld20-aosp.h
+++ u-boot/uniphier-ld20-aosp.h
@@ -3,6 +3,7 @@
 
 #include <configs/uniphier.h>
 
+#undef CONFIG_PREBOOT
 #define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0x80900000 0xe00 0x200 && md5sum -v 0x80900000 3f788 d9fe02f852ac6ff7cb217b11c3027dab && icache flush && dcache flush && go 0x80900000 1920x1080 - 0xc0000000"
 
 #endif
