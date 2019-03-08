26eb87a9705c319799122650077cbd67a2f9560f
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2019-03-08 15:03:28 +0900

    Update u-boot header to modify command-line for latest VOC firmware
    Latest VOC firmware allows new command-line format, and at the same time
    we need to specify the kind of the board to apply the firmware.
    
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

--- u-boot/uniphier-ld20-aosp.h
+++ u-boot/uniphier-ld20-aosp.h
@@ -4,6 +4,6 @@
 #include <configs/uniphier.h>
 
 #undef CONFIG_PREBOOT
-#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0x80900000 0xe00 0x200 && md5sum -v 0x80900000 3fca8 a3a7dddb71ce0dc05145cbc78f6a8f1a && icache flush && dcache flush && go 0x80900000 1920x1080 - 0xc0000000"
+#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0x80900000 0xe00 0x200 && md5sum -v 0x80900000 3fca8 a3a7dddb71ce0dc05145cbc78f6a8f1a && icache flush && dcache flush && go 0x80900000 reso=1920x1080 fb=0xc0000000 board=1"
 
 #endif
