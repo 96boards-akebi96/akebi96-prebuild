9f19b82d3c2db2e9a1b4d1001d54be2abbd02eef
Masami Hiramatsu <masami.hiramatsu@linaro.org>
2019-03-28 17:39:16 +0900

    voc-firmware: Compress with gzip to fit into MMC boot area
    Since the latest VOC firmware is a bit bigger than a space
    on MMC boot area, compress it with gzip and expand it on
    boot.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@linaro.org>

--- u-boot/uniphier-ld20-aosp.h
+++ u-boot/uniphier-ld20-aosp.h
@@ -4,6 +4,6 @@
 #include <configs/uniphier.h>
 
 #undef CONFIG_PREBOOT
-#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0x80900000 0xe00 0x200 && md5sum -v 0x80900000 3fca8 a3a7dddb71ce0dc05145cbc78f6a8f1a && icache flush && dcache flush && go 0x80900000 reso=1920x1080 fb=0xc0000000 board=1"
+#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0xc0000000 0xe00 0x200 && unzip 0xc0000000 0x80900000 && md5sum -v 0x80900000 44b08 cd88703140529e57c388beb0988c24bd && icache flush && dcache flush && go 0x80900000 reso=1920x1080 fb=0xc0000000 board=1"
 
 #endif
