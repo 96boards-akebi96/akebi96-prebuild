11b07b9b07a8a2d1fc6dfe8797da6fd5b9cdc078
Masami Hiramatsu <masami.hiramatsu@linaro.org>
2019-05-21 15:47:32 +0900

    u-boot: Clear framebuffer before run VOC-firmware
    Clear framebuffer with 0xffffffff (white) before run VOC-firmware.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@linaro.org>

--- u-boot/uniphier-ld20-aosp.h
+++ u-boot/uniphier-ld20-aosp.h
@@ -4,6 +4,6 @@
 #include <configs/uniphier.h>
 
 #undef CONFIG_PREBOOT
-#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0xc0000000 0xe00 0x200 && unzip 0xc0000000 0x80900000 && md5sum -v 0x80900000 44b08 cd88703140529e57c388beb0988c24bd && icache flush && dcache flush && go 0x80900000 reso=1920x1080 fb=0xc0000000 board=1"
+#define CONFIG_PREBOOT "mmc dev 0 1 && mmc read 0xc0000000 0xe00 0x200 && unzip 0xc0000000 0x80900000 && mw.w 0xc0000000 0xffffffff 0x200000 && md5sum -v 0x80900000 44b08 cd88703140529e57c388beb0988c24bd && icache flush && dcache flush && go 0x80900000 reso=1920x1080 fb=0xc0000000 board=1"
 
 #endif
